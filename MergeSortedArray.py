4f3a0633cf179820ab1002241b1dd6ac589aa1ce
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-13 10:08:23 +0300

    Merging a sorted array in place

---
+++ MergeSortedArray.py
@@ -0,0 +1,12 @@
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:] = nums2
+        nums1.sort()
