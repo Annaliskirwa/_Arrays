f0b3bc7b430538bb488ad7e9b62ca6bc3f5eacbc
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-14 19:55:15 +0300

    Sorting Array by Parity in Place

--- SortArrayParity.py
+++ SortArrayParity.py
@@ -11,3 +11,22 @@ class Solution(object):
     def sortArrayByParity(self, A):
         return ([x for x in A if x % 2 == 0] +
                 [x for x in A if x % 2 == 1])
+
+
+
+# In-Place
+# Intuition
+
+# If we want to do the sort in-place, we can use quicksort, a standard textbook algorithm.
+
+class Solution(object):
+    def sortArrayByParity(self, A):
+        i, j = 0, len(A) - 1
+        while i < j:
+            if A[i] % 2 > A[j] % 2:
+                A[i], A[j] = A[j], A[i]
+
+            if A[i] % 2 == 0: i += 1
+            if A[j] % 2 == 1: j -= 1
+
+        return A
