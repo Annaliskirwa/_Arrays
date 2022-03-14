6aca01a6b8f495e329497311fdf6661ff874870f
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-14 16:18:29 +0300

    Tranverse valid mountain from left n right

---  ValidMountainArray.py
+++  ValidMountainArray.py
@@ -9,3 +9,25 @@ class Solution:
         for j in range(len(arr)-1):
             if arr[j]<=arr[j+1]: return False
         return True
+
+
+
+# Tranverse array from both sides
+class Solution(object):
+    def validMountainArray(self, A):
+        N = len(A)
+        i = 0
+
+        # walk up
+        while i+1 < N and A[i] < A[i+1]:
+            i += 1
+
+        # peak can't be first or last
+        if i == 0 or i == N-1:
+            return False
+
+        # walk down
+        while i+1 < N and A[i] > A[i+1]:
+            i += 1
+
+        return i == N-1
