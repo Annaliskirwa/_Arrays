c75119cac963e387f165d48eeead3e2dc3ed57a4
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-03 22:38:01 +0300

    Remove elements using two pointers

--- StringsAndArrays/RemoveElement.py
+++ StringsAndArrays/RemoveElement.py
@@ -11,3 +11,11 @@ class Solution:
 
 
 # using pointers to remove elements in an array
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[count] = nums[i]
+                count +=1
+        return count
