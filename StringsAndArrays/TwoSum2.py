680eda8dfc54eb3e686c6c32dcea4274c142359e
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-05 23:31:30 +0300

    Retrun two sum index + 1

--- StringsAndArrays/TwoSum2.py
+++ StringsAndArrays/TwoSum2.py
@@ -10,3 +10,19 @@ class Solution:
                 return [a.index(x),i]
             else:
                 a.append(nums[i])
+
+
+
+# Two sum 2
+
+# Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        a = list()
+        for i in range(len(numbers)):
+            x = target - numbers[i]
+            if x in a:
+                return [a.index(x)+1,i+1]
+            else:
+                a.append(numbers[i])
