dc1e0bb17d5b79c0b89c1b9ab50bef6dbbb0f9b2
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-07 23:05:32 +0300

    MoMoe Zeroes

---
+++ StringsAndArrays/MoveZeroes.py
@@ -0,0 +1,13 @@
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)-1, -1, -1):
+            if (nums[i] == 0):
+                nums.pop(i)
+                nums.append(0)
